98f358f00a72600b0853bb641d4877bb34f989bd
EC2 Default User <ec2-user@ip-172-31-82-139.ec2.internal>
2022-03-01 05:05:45 +0000

    応用課題５

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,5 +38,17 @@ class User < ApplicationRecord
   end
    validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
    validates :introduction, length: { maximum: 50 }
-  
+   def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+   end
 end
